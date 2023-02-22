5dc4c46465d4d710f2540574ee7e4514e94fa3ba
rahulio96 <rahgupta96@gmail.com>
2023-02-21 17:29:49 -0800

    I hope this works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPrompt from "./components/loginprompt";
+import SignUp from "./components/signup";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddPatientButton from './components/addButton';
+
+import Works from './components/success';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<LoginPrompt />} />
+          <Route path = "/SignUp" element = {<SignUp />} />
+          <Route path = "/AddPatientButton" element = {<AddPatientButton />} />
+          <Route path = "/success" element = {<Works />} />
+        </Routes>
+  </BrowserRouter>
   );
 }
 
